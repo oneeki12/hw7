e6ae21747122e56074f7dc33c660065b59989337
oneeki12 <63128777+oneeki12@users.noreply.github.com>
2022-03-01 08:18:22 +0600

    Delete hw6_1 directory

--- hw6_1/parser_app/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Film(models.Model):
-    title = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='')
-    year = models.CharField(max_length=4)
-
